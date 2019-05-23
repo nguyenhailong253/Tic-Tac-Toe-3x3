f1d7ba9a6e13824b2a0a051d59b90aae1cdaf7bb
Long Nguyen <nguyenhailong253@gmail.com>
2019-05-23 22:41:36 +1000

    add skeleton code for messages

--- src/messages.py
+++ src/messages.py
@@ -1,1 +1,20 @@
 # author: long nguyen (nguyenhailong253@gmail.com)
+
+
+class Messages(object):
+    ''' All messages needed for TicTacToe '''
+
+    def __init__(self):
+        pass
+
+    def win_message(self):
+        pass
+
+    def draw_message(self):
+        pass
+
+    def coordinates_out_of_range(self):
+        pass
+
+    def wrong_type_input(self):
+        pass
